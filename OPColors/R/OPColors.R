0eda0763a7c3d5757357d24482dbe67001824511
Carl Romer <63623758+cromer043@users.noreply.github.com>
2024-10-21 13:07:30 -0700

    Add files via upload

--- OPColors/R/OPColors.R
+++ OPColors/R/OPColors.R
@@ -5,7 +5,7 @@
 #This file copies code from jcolor https://jaredhuling.org/jcolors/
 #To apply Oakland Promise Color Palates
 ###############################
-# OPcolors
+# OPColors
 
 #' Vectors of colors for figures
 #'
@@ -21,28 +21,28 @@
 #'      xaxt = "n", yaxt = "n", xaxs = "i")
 #' axis(side=3, at=1:3, c("default", "pal2", "pal3"), las=1)
 #'
-#' def <- OPcolors("default")
+#' def <- OPColors("default")
 #' points(seq(along = def), rep(1, length(def)), pch = 22, bg = def, cex = 8)
-#' pal2 <- OPcolors("pal2")
+#' pal2 <- OPColors("pal2")
 #' points(seq(along = pal2), rep(2, length(pal2)), pch = 22, bg = pal2, cex = 8)
-#' pal3 <- OPcolors("pal3")
+#' pal3 <- OPColors("pal3")
 #' points(seq(along = pal3), rep(3, length(pal3)), pch = 22, bg = pal3, cex = 8)
-OPcolors <- function(palette = c('Primary',
+OPColors <- function(palette = c('Primary',
                                  'Default' ,
                                  'Highlight',
-                                 
+
                                  'Brilliant Baby primary'  ,
                                  'Brilliant Baby default' ,
                                  'Brilliant Baby highlight'  ,
-                                 
+
                                  'K2C primary' ,
                                  'K2C default'  ,
                                  'K2C highlight',
-                                 
+
                                  'College Access primary' ,
                                  'College Access default' ,
                                  'College Access highlight' ,
-                                 
+
                                  'College Completion primary'   ,
                                  'College Completion default',
                                  'College Completion highlight'))
@@ -68,11 +68,11 @@ OPcolors <- function(palette = c('Primary',
                  )
 
     Highlight <- c('highlight' = "#31AB20")
-    
+
     `Brilliant Baby primary` <- c('BB baby blue' = '#0CA9E8',
                                  'BB blue' = '#2C4999',
                                  'BB green' = '#239337')
-    
+
     `Brilliant Baby default` <- c('BB blue 1' = '#04719C',
                                 'BB blue 2' = '#0984B5',
                                 'BB blue 3' = '#66CEF5',
@@ -85,13 +85,13 @@ OPcolors <- function(palette = c('Primary',
                                 'BB blue 5' = '#63ACE4',
                                 'BB orange' = '#FF9D80',
                                 'BB midnight' =  '#0F1F4D')
-    
+
     `Brilliant Baby highlight` <- c('BB highlight' = '#88C7F2')
-    
+
     `K2C primary` <- c('K2C baby blue' = '#3769CD',
                        'K2C blue' = '#2C4999',
-                       'K2C green' = '#239337') 
-    
+                       'K2C green' = '#239337')
+
     `K2C default` <- c('K2C pink' = '#E2A0E8',
                        'K2C teal' = '#2DB6CC',
                        'K2C sky blue' = '#A6e0e4',
@@ -101,13 +101,13 @@ OPcolors <- function(palette = c('Primary',
                        'K2C yellow' = '#fddc7e',
                        'K2C brown' = '#C3A039',
                        'K2C midnight' = '#0F1F4D')
-    
+
     `K2C highlight` <- c('K2C highlight' = '#329ce3')
-    
+
     `College Access primary` <- c('CA green' = '#31AB20',
                                   'CA blue' = '#2C4999',
                                   'CA forest' = '#239337')
-    
+
     `College Access default` <- c('CA olive' = '#8FCC5A',
                                  'CA light green' = '#CFEFA6',
                                  'CA dark green' = '#294536',
@@ -119,13 +119,13 @@ OPcolors <- function(palette = c('Primary',
                                  'CA orange' = '#F7AD23',
                                  'CA pink' = '#AB0F77',
                                  'CA brown' = '#AB7718')
-    
+
     `College Access highlight` <- c('CA highlight' = '#8FCC5A')
-    
+
     `College Completion primary` <- c('CC Purple' = '#5D41CC',
                                   'CC blue' = '#2C4999',
                                   'CC green' = '#239337')
-    
+
     `College Completion default` <- c('CC sky blue' = '#94f5ff',
                                  'CC purple' = '#896BFF',
                                  'CC orange' = '#FFC238',
@@ -138,37 +138,37 @@ OPcolors <- function(palette = c('Primary',
                                  'CC olive' = '#9ccc37',
                                  'CC blood orange' = '#cc6856',
                                  'CC blue' =  '#0ca9e8')
-    
+
     `College Completion highlight` <- c('CC highlight' = '#896bff')
 
     switch(match.arg(palette),
-           
+
            `Primary`   = `Primary`,
            `Default`   = `Default`,
            `Highlight`   = `Highlight`,
-           
+
            `Brilliant Baby primary`   = `Brilliant Baby primary`,
            `Brilliant Baby default`   = `Brilliant Baby default`,
            `Brilliant Baby highlight`   = `Brilliant Baby highlight`,
-           
+
            `K2C primary`   = `K2C primary`,
            `K2C default`   = `K2C default`,
            `K2C highlight`   = `K2C highlight`,
-           
+
            `College Access primary`   = `College Access primary`,
            `College Access default`   = `College Access default`,
            `College Access highlight`   = `College Access highlight`,
-           
+
            `College Completion primary`   = `College Completion primary`,
            `College Completion default` = `College Completion default`,
            `College Completion highlight` = `College Completion highlight`)
 }
 
 
-# display.OPcolors
-#' Display OPcolors
+# display.OPColors
+#' Display OPColors
 #'
-#' displays the discrete OPcolors palettes
+#' displays the discrete OPColors palettes
 #'
 #' @param palette Character string indicating a palette of colors.
 #' @export
@@ -177,29 +177,29 @@ OPcolors <- function(palette = c('Primary',
 #' @importFrom graphics text
 #' @importFrom graphics par
 #' @examples
-#' display_OPcolors()
-display_OPcolors <- function(palette = c('Primary',
+#' display_OPColors()
+display_OPColors <- function(palette = c('Primary',
                                          'Default' ,
                                          'Highlight',
-                                         
+
                                          'Brilliant Baby primary'  ,
                                          'Brilliant Baby default' ,
                                          'Brilliant Baby highlight'  ,
-                                         
+
                                          'K2C primary' ,
                                          'K2C default'  ,
                                          'K2C highlight',
-                                         
+
                                          'College Access primary' ,
                                          'College Access default' ,
                                          'College Access highlight' ,
-                                         
+
                                          'College Completion primary'   ,
                                          'College Completion default',
                                          'College Completion highlight'))
 {
     palette <- match.arg(palette)
-    jcols   <- OPcolors(palette)
+    jcols   <- OPColors(palette)
     n       <- length(jcols)
     cnames  <- names(jcols)
     image(1:n, 1, as.matrix(1:n),
@@ -211,8 +211,8 @@ display_OPcolors <- function(palette = c('Primary',
          labels = cnames, xpd = TRUE)
 }
 
-# display all OPcolors
-#' Display all OPcolors
+# display all OPColors
+#' Display all OPColors
 #'
 #' Creates different vectors of related colors that may be useful for figures.
 #'
@@ -222,29 +222,29 @@ display_OPcolors <- function(palette = c('Primary',
 #' @importFrom graphics axis
 #' @importFrom graphics plot
 #' @examples
-#' display_all_OPcolors()
-display_all_OPcolors <- function()
+#' display_all_OPColors()
+display_all_OPColors <- function()
 {
     palette <- c('Primary',
                  'Default' ,
                  'Highlight',
-                 
+
                  'Brilliant Baby primary'  ,
                  'Brilliant Baby default' ,
                  'Brilliant Baby highlight'  ,
-                 
+
                  'K2C primary' ,
                  'K2C default'  ,
                  'K2C highlight',
-                 
+
                  'College Access primary' ,
                  'College Access default' ,
                  'College Access highlight' ,
-                 
+
                  'College Completion primary'   ,
                  'College Completion default',
                  'College Completion highlight')
-    jcols    <- sapply(palette, OPcolors)
+    jcols    <- sapply(palette, OPColors)
     maxlen   <- max(sapply(jcols, length))
     ncols    <- length(jcols)
 
@@ -259,7 +259,7 @@ display_all_OPcolors <- function()
 
     for (c in 1:ncols)
     {
-        pal <- OPcolors(palette[c])
+        pal <- OPColors(palette[c])
         #points(seq(along = pal), rep(yseq[c], length(pal)), pch = 22, bg = pal, cex = 6)
         rect(xleft   = seq(along = pal) - 0.5,
              ybottom = yseq[c] - 0.85,
@@ -273,14 +273,14 @@ display_all_OPcolors <- function()
 
 
 
-#' OPcolors color scales
+#' OPColors color scales
 #'
-#' @inheritParams OPcolors
+#' @inheritParams OPColors
 #' @param ... additional parameters for \code{\link[ggplot2]{discrete_scale}}
-#' @export scale_color_OPcolors
+#' @export scale_color_OPColors
 #' @importFrom ggplot2 discrete_scale
 #' @importFrom scales manual_pal
-#' @rdname scale_OPcolors
+#' @rdname scale_OPColors
 #'
 #' @examples
 #' library(ggplot2)
@@ -304,27 +304,27 @@ display_all_OPcolors <- function()
 #'           panel.grid.major = element_line(color = "grey45"),
 #'           panel.grid.minor = element_line(color = "grey25"))
 #'
-#' pltl + scale_color_OPcolors(palette = "default")
+#' pltl + scale_color_OPColors(palette = "default")
 #'
-#' pltd + scale_color_OPcolors(palette = "default")
+#' pltd + scale_color_OPColors(palette = "default")
 #'
 #'
-scale_color_OPcolors = function (palette = c('Primary',
+scale_color_OPColors = function (palette = c('Primary',
                                              'Default' ,
                                              'Highlight',
-                                             
+
                                              'Brilliant Baby primary'  ,
                                              'Brilliant Baby default' ,
                                              'Brilliant Baby highlight'  ,
-                                             
+
                                              'K2C primary' ,
                                              'K2C default'  ,
                                              'K2C highlight',
-                                             
+
                                              'College Access primary' ,
                                              'College Access default' ,
                                              'College Access highlight' ,
-                                             
+
                                              'College Completion primary'   ,
                                              'College Completion default',
                                              'College Completion highlight'),
@@ -332,35 +332,35 @@ scale_color_OPcolors = function (palette = c('Primary',
 {
     palette <- match.arg(palette)
     discrete_scale("colour",
-                   "OPcolors",
-                   manual_pal(unname(OPcolors(palette))),
+                   "OPColors",
+                   manual_pal(unname(OPColors(palette))),
                    ...)
 }
 
-#' @export scale_colour_OPcolors
-#' @rdname scale_OPcolors
-scale_colour_OPcolors = scale_color_OPcolors
+#' @export scale_colour_OPColors
+#' @rdname scale_OPColors
+scale_colour_OPColors = scale_color_OPColors
 
 
-#' @export scale_fill_OPcolors
+#' @export scale_fill_OPColors
 #' @importFrom ggplot2 discrete_scale
-#' @rdname scale_OPcolors
-scale_fill_OPcolors = function (palette = c('Primary',
+#' @rdname scale_OPColors
+scale_fill_OPColors = function (palette = c('Primary',
                                             'Default' ,
                                             'Highlight',
-                                            
+
                                             'Brilliant Baby primary'  ,
                                             'Brilliant Baby default' ,
                                             'Brilliant Baby highlight'  ,
-                                            
+
                                             'K2C primary' ,
                                             'K2C default'  ,
                                             'K2C highlight',
-                                            
+
                                             'College Access primary' ,
                                             'College Access default' ,
                                             'College Access highlight' ,
-                                            
+
                                             'College Completion primary'   ,
                                             'College Completion default',
                                             'College Completion highlight'),
@@ -368,8 +368,8 @@ scale_fill_OPcolors = function (palette = c('Primary',
 {
     palette <- match.arg(palette)
     discrete_scale("fill",
-                   "OPcolors",
-                   manual_pal(unname(OPcolors(palette))),
+                   "OPColors",
+                   manual_pal(unname(OPColors(palette))),
                    ...)
 }
 
